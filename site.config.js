4d8875c49129883b4f61ee6c04825b1e106f74ad
HiroNewf <64501695+HiroNewf@users.noreply.github.com>
2024-02-12 13:21:47 -0500

    site.config.js

--- site.config.js
+++ site.config.js
@@ -2,10 +2,10 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "HiroNewf",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role:"",
     bio: "Professional Googler pretending to be an Ethical Hacker. PNPT, Sec+, & Net+ Certified",
-    email: "morethanmin.dev@gmail.com",
+    email: "HiroNewf@protonmail.com",
     linkedin: "",
     github: "HiroNewf",
     instagram: "",
@@ -23,7 +23,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://blog-hironewfs-projects.vercel.app/",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
